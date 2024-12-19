9024aa44ef7da8fa5a6d322f95009490ef86af8a
Muneer Hashmat <muneerhashmat@gmail.com>
2024-12-19 17:07:20 +0530

    added proper icons to chatbot component

--- background.js
+++ background.js
@@ -1,40 +1,39 @@
 chrome.tabs.onCreated.addListener(async (tab) => {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        if (tabs[0].pendingUrl === "https://www.linkedin.com/my-items/saved-posts/") {
-            const id = tabs[0].id;
-            let message = { action: "execute" };
-            setTimeout(() => {
-                chrome.tabs.sendMessage(tabs[0].id, message);
-            }, 4000)
-        }
-    });
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    if (
+      tabs[0].pendingUrl === "https://www.linkedin.com/my-items/saved-posts/"
+    ) {
+      const id = tabs[0].id;
+      let message = { action: "execute" };
+      setTimeout(() => {
+        chrome.tabs.sendMessage(tabs[0].id, message);
+      }, 4000);
+    }
+  });
 });
 
-const apiKey = "AIzaSyAEMelXC-Z12ddh6xBBAG-vWi1oK-bK9O4";
+const apiKey = "AIzaSyAk540Dpw8zIrm-0lNvM1cR8dEMrueoysA";
 import { GoogleGenerativeAI } from "./scripts/index.mjs";
 const genAI = new GoogleGenerativeAI(apiKey);
 
-
 chrome.runtime.onMessage.addListener(async (message) => {
-    if (message.action === 'sendText') {
-        const prompt = message.data;
-        sendResponse(prompt);
-    }
+  if (message.action === "sendText") {
+    const prompt = message.data;
+    sendResponse(prompt);
+  }
 });
 
 async function sendResponse(prompt) {
-    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
-    const result = await model.generateContent(prompt);
-    const response = await result.response;
-    const markdownText = response.text();
-    console.log(markdownText);
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        const id = tabs[0].id;
-        let message = { action: "textSent", data: markdownText };
-        setTimeout(() => {
-            chrome.tabs.sendMessage(tabs[0].id, message);
-        }, 3000)
-    });
-
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-8b" });
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const markdownText = response.text();
+  console.log(markdownText);
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    const id = tabs[0].id;
+    let message = { action: "textSent", data: markdownText };
+    setTimeout(() => {
+      chrome.tabs.sendMessage(tabs[0].id, message);
+    }, 3000);
+  });
 }
-
--- scripts/content.js
+++ scripts/content.js
@@ -18,17 +18,20 @@ target="_blank" href="https://www.linkedin.com/my-items/saved-posts/" id="anchor
 </div>
 <span class="t-12 break-words block t-black--light t-normal global-nav__primary-link-text">Saved Posts</span>
 </a> 
-`
-
-
+`;
 
 //creating chatbot open button
 let chatBotToggle = document.createElement("button");
 chatBotToggle.setAttribute("id", "chatbot-toggle");
+const botImgPath = chrome.runtime.getURL("images/robot.png");
+const closeImgPath = chrome.runtime.getURL("images/close.png");
+const userImgPath = chrome.runtime.getURL("images/account.png");
+const sendImgPath = chrome.runtime.getURL("images/message.png");
+const loaderImgPath = chrome.runtime.getURL("images/ellipse.gif");
 chatBotToggle.innerHTML = `
-<span class="img">🤖</span>
-<span class="img">✖️</span>
-`
+<span class="img" style="padding-top: 10px;"><img src="${botImgPath}" alt = "bot" width = "30px" height = "35px"></span>
+<span class="img"><img src="${closeImgPath}" alt = "close" width = "28px" height = "20px"></span>
+`;
 
 //creating chatbot window
 let chatBot = document.createElement("div");
@@ -41,7 +44,7 @@ chatBot.innerHTML = `
 
         <div class="chatbox">
             <div class="bot-text">
-            <span class="chat-img">🤖</span>
+            <span class="chat-img"><img src="${botImgPath}" alt = "mic" width = "25px" height = "25px"></span>
                 <div>Hi there 👋 I'm your linkedIn chatbot <br>How can I help you today</div>
             </div>
         </div>
@@ -51,77 +54,62 @@ chatBot.innerHTML = `
         <div class="form">
             <textarea placeholder="Enter a prompt...." spellcheck="false" required></textarea>
 
-            <button>▶️</button>
+            <button><img src="${sendImgPath}" alt = "mic" width = "30px" height = "25px"></button>
         </div>
-`
+`;
 
 let typing = document.createElement("div");
 typing.classList.add("bot-text");
 typing.innerHTML = `
-     <span class="chat-img"></span>
-    <div>Generating.....</div>
-`
-
-
+     <span class="chat-img"><img src="${botImgPath}" alt = "mic" width = "25px" height = "25px"></span>
+    <div id="loader"><img src="${loaderImgPath}" alt = "loader" height = "35px"></div>
+`;
 
 //function to append newly created ui elements to the page
 function run() {
-    linksContainer = document.querySelector("ul.global-nav__primary-items");
-    linksContainer.appendChild(savedPostsLink);
-    body = document.querySelector("body");
-    body.appendChild(speakButton);
-    body.appendChild(chatBotToggle);
-    body.appendChild(chatBot);
+  linksContainer = document.querySelector("ul.global-nav__primary-items");
+  linksContainer.appendChild(savedPostsLink);
+  body = document.querySelector("body");
+  body.appendChild(speakButton);
+  body.appendChild(chatBotToggle);
+  body.appendChild(chatBot);
 }
 
 try {
-    run();
+  run();
+} catch (err) {
+  console.log(err);
 }
-catch (err) {
-    console.log(err);
-}
-
 
 //listening to messages from popup and background scripts
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    if (message.action === "visible") {
-        savedPostsLink.style.display = "block";
-    }
-    else if (message.action === "invisible") {
-        savedPostsLink.style.display = "none";
-    }
-
-    else if (message.action === "visible2") {
-        speakButton.style.display = "flex";
-    }
-    else if (message.action === "invisible2") {
-        speakButton.style.display = "none";
-    }
-    else if (message.action === "visible3") {
-        chatBotToggle.style.display = "flex";
-    }
-    else if (message.action === "invisible3") {
-        chatBotToggle.style.display = "none";
-    }
-    else if (message.action === "execute") {
-        run();
-    }
-    else if (message.action === "textSent") {
-        const markdownText = message.data;
-        const md = window.markdownit();
-        const text = md.render(markdownText);
-        chatBot.children[1].removeChild(typing);
-        let botResponse = document.createElement("div");
-        botResponse.classList.add("bot-text");
-        botResponse.innerHTML = `
-        <span class="chat-img">🤖</span>
-        <div>${text}</div>`
-        chatBot.children[1].appendChild(botResponse);
-    }
+  if (message.action === "visible") {
+    savedPostsLink.style.display = "block";
+  } else if (message.action === "invisible") {
+    savedPostsLink.style.display = "none";
+  } else if (message.action === "visible2") {
+    speakButton.style.display = "flex";
+  } else if (message.action === "invisible2") {
+    speakButton.style.display = "none";
+  } else if (message.action === "visible3") {
+    chatBotToggle.style.display = "flex";
+  } else if (message.action === "invisible3") {
+    chatBotToggle.style.display = "none";
+  } else if (message.action === "execute") {
+    run();
+  } else if (message.action === "textSent") {
+    const markdownText = message.data;
+    const md = window.markdownit();
+    const text = md.render(markdownText);
+    chatBot.children[1].removeChild(typing);
+    let botResponse = document.createElement("div");
+    botResponse.classList.add("bot-text");
+    botResponse.innerHTML = `
+        <span class="chat-img"><img src="${botImgPath}" alt = "mic" width = "25px" height = "25px"></span>
+        <div>${text}</div>`;
+    chatBot.children[1].appendChild(botResponse);
+  }
 });
-
-
-
 
 speakButton.setAttribute("id", "speak-button");
 speakButton.classList.add("line");
@@ -129,7 +117,7 @@ speakButton.classList.add("line");
 const micImagePath = chrome.runtime.getURL("images/mic.png");
 speakButton.innerHTML = `
 <img src="${micImagePath}" alt = "mic" width = "30px" height = "28px">
-`
+`;
 
 //speech to text
 let speechRecognition = new webkitSpeechRecognition();
@@ -139,131 +127,111 @@ speechRecognition.lang = "en-us";
 
 // let anchorTag = document.getElementById("anchor-tag");
 speechRecognition.onresult = (e) => {
-    console.log(e);
-    let savedPostCommand = e.results[e.resultIndex][0].transcript;
-    console.log(savedPostCommand);
-    let text = savedPostCommand.trim().toLowerCase();
-    if (text.includes("open saved posts")) {
-        // anchorTag.click();
-        linksContainer.children[8].children[0].click();
-        speakButton.click();
-        return;
-    }
-    else if (text.includes("open home")) {
-        linksContainer.children[0].children[0].click();
-    }
-    else if (text.includes("open my network")) {
-        linksContainer.children[1].children[0].click();
-    }
-    else if (text.includes("open jobs")) {
-        linksContainer.children[2].children[0].click();
-    }
-    else if (text.includes("open messag")) {
-        linksContainer.children[3].children[0].click();
-    }
-    else if (text.includes("open notification")) {
-        linksContainer.children[4].children[0].click();
-    }
-    else if (text.includes("open notification")) {
-        linksContainer.children[5].children[0].click();
-    }
-}
+  console.log(e);
+  let savedPostCommand = e.results[e.resultIndex][0].transcript;
+  console.log(savedPostCommand);
+  let text = savedPostCommand.trim().toLowerCase();
+  if (text.includes("open saved posts")) {
+    // anchorTag.click();
+    linksContainer.children[8].children[0].click();
+    speakButton.click();
+    return;
+  } else if (text.includes("open home")) {
+    linksContainer.children[0].children[0].click();
+  } else if (text.includes("open my network")) {
+    linksContainer.children[1].children[0].click();
+  } else if (text.includes("open jobs")) {
+    linksContainer.children[2].children[0].click();
+  } else if (text.includes("open messag")) {
+    linksContainer.children[3].children[0].click();
+  } else if (text.includes("open notification")) {
+    linksContainer.children[4].children[0].click();
+  } else if (text.includes("open notification")) {
+    linksContainer.children[5].children[0].click();
+  }
+};
 
 speakButton.addEventListener("click", () => {
-    if (!speakButton.hasAttribute("listening")) {
-        speakButton.setAttribute("listening", true);
-        speakButton.classList.remove("line");
-        speechRecognition.start();
-    }
-    else {
-        speakButton.removeAttribute("listening");
-        speakButton.classList.add("line");
-        speechRecognition.stop();
-    }
-})
-
-
+  if (!speakButton.hasAttribute("listening")) {
+    speakButton.setAttribute("listening", true);
+    speakButton.classList.remove("line");
+    speechRecognition.start();
+  } else {
+    speakButton.removeAttribute("listening");
+    speakButton.classList.add("line");
+    speechRecognition.stop();
+  }
+});
 
 //showing saved posts link, speak button and bot button based on the state of buttons in popup.html
 savedPostsLink.style.display = "none";
 speakButton.style.display = "none";
 chatBotToggle.style.display = "none";
 
-
 chrome.storage.sync.get("buttonState1", function (data) {
-    if (data.buttonState1) {
-        savedPostsLink.style.display = "block";
-    }
+  if (data.buttonState1) {
+    savedPostsLink.style.display = "block";
+  }
 });
 
 chrome.storage.sync.get("buttonState2", function (data) {
-    if (data.buttonState2) {
-        speakButton.style.display = "flex";
-    }
+  if (data.buttonState2) {
+    speakButton.style.display = "flex";
+  }
 });
 
 chrome.storage.sync.get("buttonState3", function (data) {
-    if (data.buttonState3) {
-        chatBotToggle.style.display = "flex";
-    }
+  if (data.buttonState3) {
+    chatBotToggle.style.display = "flex";
+  }
 });
-
-
 
 //showing chatbot upon clicking chatbot toggle button
 chatBotToggle.children[0].classList.add("visible");
 chatBotToggle.addEventListener("click", () => {
+  if (!chatBot.hasAttribute("visible")) {
+    chatBot.style.display = "block";
+    chatBot.setAttribute("visible", true);
+  } else {
+    chatBot.style.display = "none";
+    chatBot.removeAttribute("visible");
+  }
 
-    if (!chatBot.hasAttribute("visible")) {
-        chatBot.style.display = "block";
-        chatBot.setAttribute("visible", true);
-    }
-    else {
-        chatBot.style.display = "none";
-        chatBot.removeAttribute("visible");
-    }
-
-    if (!chatBotToggle.hasAttribute("click")) {
-        chatBotToggle.setAttribute("click", true);
-        chatBotToggle.children[0].classList.remove("visible");
-        chatBotToggle.children[1].classList.add("visible");
-    }
-    else {
-        chatBotToggle.removeAttribute("click");
-        chatBotToggle.children[1].classList.remove("visible");
-        chatBotToggle.children[0].classList.add("visible");
-    }
-
-})
-
-
-
+  if (!chatBotToggle.hasAttribute("click")) {
+    chatBotToggle.setAttribute("click", true);
+    chatBotToggle.children[0].classList.remove("visible");
+    chatBotToggle.children[1].classList.add("visible");
+  } else {
+    chatBotToggle.removeAttribute("click");
+    chatBotToggle.children[1].classList.remove("visible");
+    chatBotToggle.children[0].classList.add("visible");
+  }
+});
 
 function chatHandler() {
-    let textInput = chatBot.children[3].children[0].value.trim();
-    chatBot.children[3].children[0].value = "";
-    if (!textInput) {
-        return;
-    }
-    let newInput = document.createElement("div");
-    newInput.classList.add("user-text");
-    newInput.innerHTML = `
+  let textInput = chatBot.children[3].children[0].value.trim();
+  chatBot.children[3].children[0].value = "";
+  if (!textInput) {
+    return;
+  }
+  let newInput = document.createElement("div");
+  newInput.classList.add("user-text");
+  newInput.innerHTML = `
             <div> <span>${textInput}</span></div>
-                <span class="chat-img2">💻</span>
-        `
-    chatBot.children[1].appendChild(newInput);
+                <span class="chat-img2"><img src="${userImgPath}" alt = "mic" width = "25px" height = "25px"></span>
+        `;
+  chatBot.children[1].appendChild(newInput);
 
-
-    //sending message to background script
-    chrome.runtime.sendMessage({ action: 'sendText', data: textInput });
-    chatBot.children[1].appendChild(typing);
+  //sending message to background script
+  chrome.runtime.sendMessage({ action: "sendText", data: textInput });
+  chatBot.children[1].appendChild(typing);
 }
 
 chatBot.children[3].children[1].addEventListener("click", chatHandler);
 
 chatBot.children[3].children[0].addEventListener("keydown", (e) => {
-    if (e.key === "Enter" && !e.shiftKey) {
-        e.preventDefault();
-        chatHandler();
-    }
+  if (e.key === "Enter" && !e.shiftKey) {
+    e.preventDefault();
+    chatHandler();
+  }
 });
